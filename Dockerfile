1c843401c873ef385d61319b92eeb9c5c1d13f63
arigatu <alek-ardashev@yandex.ru>
2021-01-01 15:41:17 +0300

    metricsss

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ COPY . ./
 
 # --------------- Install python packages using `pip` ---------------
 
-RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt \
+RUN pip3 install --upgrade pip && pip3 install --no-cache-dir -r requirements.txt \
 	&& rm -rf requirements.txt
 
 # --------------- Configure Streamlit ---------------
--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def dataset(n):
 
     df_init = pandas_gbq.read_gbq(sql_query, project_id=project_id)
     df_init['ds'] = df_init['ds'].dt.strftime('%Y-%m-%d')'''
-    df_init = pd.read_csv('gs://metricsss/installs.csv')
+    df_init = pd.read_csv('gs://axiomm/installs.csv')
     df_init.drop(df_init.tail(n).index, inplace=True)
     return df_init
 
@@ -92,7 +92,7 @@ def anomaly(data_new, forecast_old):
     df_merged["Actual value"] = df_merged["Actual value"].fillna("0").astype(int)
 
     df_merged.to_csv('forecast_merged.csv')
-    storage_client.get_bucket('metricsss').blob('forecast_merged.csv').upload_from_filename(
+    storage_client.get_bucket('axiomm').blob('forecast_merged.csv').upload_from_filename(
         'forecast_merged.csv')
     return df_merged
 
@@ -113,13 +113,13 @@ def color_survived(val):
 
 def main():
 
-    if is_exist('metricsss','forecast.csv') == False:
-            #os.path.exists('gs://metricsss/forecast.csv'):
+    if is_exist('axiomm','forecast.csv') == False:
+            #os.path.exists('gs://axiomm/forecast.csv'):
         data_first = dataset(1)
         forecast_for_today = prediction(data_first)[0]
         #gstorage
         forecast_for_today.to_csv('forecast.csv')
-        storage_client.get_bucket('metricsss').blob('forecast.csv').upload_from_filename('forecast.csv')
+        storage_client.get_bucket('axiomm').blob('forecast.csv').upload_from_filename('forecast.csv')
         main()
     else:
 
@@ -127,7 +127,7 @@ def main():
         #daily_iterations
         data_new = dataset(0)
         #gstorage
-        forecast_for_today = pd.read_csv('gs://metricsss/forecast.csv')
+        forecast_for_today = pd.read_csv('gs://axiomm/forecast.csv')
         forecast_for_tomorrow = prediction(data_new)[0]
         #Safe update
         last_date1 = forecast_for_today['ds'].iloc[-1]
@@ -135,16 +135,16 @@ def main():
         if last_date1 != last_date2:
             # gstorage
             forecast_for_tomorrow.to_csv('forecast.csv')
-            storage_client.get_bucket('metricsss').blob('forecast.csv').upload_from_filename(
+            storage_client.get_bucket('axiomm').blob('forecast.csv').upload_from_filename(
                 'forecast.csv')
             # gstorage
             forecast_for_today.to_csv('forecast_for_spammers.csv')
-            storage_client.get_bucket('metricsss').blob('forecast_for_spammers.csv').upload_from_filename(
+            storage_client.get_bucket('axiomm').blob('forecast_for_spammers.csv').upload_from_filename(
                 'forecast_for_spammers.csv')
         else:
             st.text('No new updates')
             # gstorage
-            forecast_for_today = pd.read_csv('gs://metricsss/forecast_for_spammers.csv')
+            forecast_for_today = pd.read_csv('gs://axiomm/forecast_for_spammers.csv')
 
 
         # output
